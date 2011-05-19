ab4e8297abe7999eb7a44ce4a1ef4b9911bb0124
Wensley <bstriegs@yahoo.com>
2011-05-19 00:57:45 -0400

    partly commented, width reduced

--- webstatus-ajax.js
+++ webstatus-ajax.js
@@ -1,17 +1,22 @@
 function MakeRequest()
 {
+  // determines whether to show "Loading games..." in the table area or below
   if(splash_screen)
   {
+    // "ajax-response" is the name of the div where the table goes
     document.getElementById("ajax-response").innerHTML = "Loading games...";
-    splash_screen = 0;
+    splash_screen = 0; // don't use booleans, they are lies
   }
   else
   {
+    // "players" is the name of the td that normally shows the number of players
     document.getElementById("players").innerHTML = "Loading games...";
   }
 
+  // reset the array that holds all the table data
   game_data = [{Player: "-", Version: "-", XL: "-", Char: "-", Place: "-", Term: "-", Idle: "-", Viewers: "-", Server: "-"}];
   
+  /* begin ajax shenanigans */
   xmlHttp = getXMLHttp();
  
   xmlHttp.onreadystatechange = function()
@@ -57,26 +62,33 @@ function getXMLHttp()
   }
   return xmlHttp;
 }
+/* end ajax shenanigans */
 
-function HandleResponse(response)
+function HandleResponse(response) // response is the string returned by ajax/php
 {
   var split_response = new Array();
-  split_response = response.split("|");
+  split_response = response.split("|"); // "|" denotes divisions between entries
   
-  var game;
+  var game; // holds all data for a single game
   
+  // populate the table data array
   for(i = 0; i < (split_response.length); i++)
   {
-    game = split_response[i].split("#");
+    game = split_response[i].split("#"); // "#" denotes divisions between fields
+    // game object has the following properties: Player (player name), XL
+    // (experience level), Char (character combo), Place (place in the dungeon),
+    // Term (terminal size), Idle (idle time), Viewers (number of viewers),
+    // Server (where each game is being played)
     game_data[i]={Player: game[0], Version: game[1], XL: ParseXL(game[2]), Char: game[3], Place: game[4], Term: game[5], Idle: game[6], Viewers: game[7], Server: game[8]};
   }
   
-  SortData();
+  SortData(); // put the data in order according to sort_category
   
-  t = setTimeout("Countdown()",3000);
-  timer_is_on = 1;
+  t = setTimeout("Countdown()",3000); // begin the countdown-to-refresh timer
+  timer_is_on = 1; // note that the timer is running
 }
 
+// used to make the XL align and sort properly
 function ParseXL(raw_xl)
 {
   var xl;
@@ -90,45 +102,59 @@ function ParseXL(raw_xl)
   return xl;
 }
 
+// put the data in order according to the global variable sort_category
 function SortData()
 {
   var sort_keys = new Array();
   var game;
   var key;
   
-  for(i = 0; i < (game_data.length); i++)
+  for(i = 0; i < (game_data.length); i++) // loops for each game in table
   {
     game = game_data[i];
-    key = FudgeNumbers(game[sort_category]) + "#" + game["Player"] + "|" + i;
-    sort_keys[i] = key.toLowerCase();
+    // example of what key might look like, for sort_category "XL":
+    // 27#elliptic#git#CDO-DGL|4
+    // the name of the player, version of the game, and server of the game
+    // are used as a fallback sort for when the sort category returns a match
+    // the player name, version, and server form the unique key
+    // the 4 denotes the position in the original unsorted array
+    key = FudgeNumbers(game[sort_category]) + "#" + game["Player"] + "#" + game["Version"] + "#" + game["Server"] + "|" + i;
+    
+    // put all the keys in an array
+    sort_keys[i] = key.toLowerCase(); // sorting is case-sensitive
   }
   
-  sort_keys.sort();
+  sort_keys.sort(); // built-in javascript sorting function
+  
+  // sort_keys now represents the table data in order
   
   var index;
   var sorted_games = new Array();
   
-  for(i = 0; i < (sort_keys.length); i++)
+  for(i = 0; i < (sort_keys.length); i++) // loop for each key
   {
-    index = sort_keys[i].split("|")[1]
-    sorted_games[i] = game_data[index];
+    // retrieve the location of the current element in the unsorted array
+    index = sort_keys[i].split("|")[1];
+    sorted_games[i] = game_data[index]; // move the element
   }
   
-  game_data = sorted_games;
+  game_data = sorted_games; // reassign the table data array
   
+  // this condition is only true when the user has sorted a column in reverse
+  // and then allows the table to refresh automatically
   if(sort_reversed)
   {
     game_data = game_data.reverse();
   }
   
-  //document.getElementById('debug').innerHTML = sorted_games[0].Player;
-  
-  CreateTable();
+  CreateTable(); // create the table!
 }
 
+// the sorting algorithm can't be numeric, so "Idle" property (which is saved as
+// raw seconds) has to be fudged to make it sort properly
 function FudgeNumbers(value)
 {
-  if(sort_category == "Idle" || sort_category == "Term")
+  if(sort_category == "Idle")
   {
     switch(value.length)
     {
@@ -155,21 +181,34 @@ function FudgeNumbers(value)
   return value;
 }
 
+// create the html table that holds all the table data
 function CreateTable()
 {
-  var table_string = "<table id='data-table'><tr id='headrow' class='headrow'>";
+  // "data-table" is what is overwritten when the countdown timer elapses
+  var table_string = "<table id='data-table'><tr>";
   
-  game = game_data[0];
+  // get the names of the categories, defined as object properties
+  game = game_data[0]; 
   
   for(i in game)
   {
-    if(i == sort_category)
+    var padded_name = i;
+    
+    if(i == "Place")
     {
-      table_string += "<th onmousedown='return false;' onselectstart='return false;' class='sort' onclick='SortCategories(\"" + i + "\");'>" + i + "</td>";
+      // max length of the Place column is 7, except maybe for portal vaults
+      padded_name = i + "&nbsp;&nbsp;";
     }
-    else if(!(i == "Term"))
+    
+    if(i == sort_category) // visual cue for the current sort category
     {
-      table_string += "<th onmousedown='return false;' onselectstart='return false;' onclick='SortCategories(\"" + i + "\")'>" + i + "</td>";
+      table_string += "<th class='sort' onmousedown='return false;' onselectstart='return false;' onclick='SortCategories(\"" + i + "\")'>" + i + "</th>";
+    }
+    else if(!(i == "Term")) // we don't want to see the term category
+    {
+      // "onmousedown='return false;' onselectstart='return false;'
+      // keep the header text from being highlightable
+      table_string += "<th onmousedown='return false;' onselectstart='return false;' onclick='SortCategories(\"" + i + "\")'>" + i + "</th>";
     }
   }
   
@@ -178,34 +217,40 @@ function CreateTable()
   game = "";
   var player_name;
   
+  // put the data into the table
   for(i = 0; i < (game_data.length); i++)
   {
     game = game_data[i];
     
     if(i % 2 == 0)
     {
-      table_string += "<tr class='norm'>";
+      table_string += "<tr class='norm'>"; // even rows are darker
     }
     else
     {
-      table_string += "<tr class='alt'>";
+      table_string += "<tr class='alt'>"; // odd rows are lighter
     }
     
     for(j in game)
     {
+      // we want the player's name to be a link to their scoring page
       if(j == "Player")
       {
         player_name = game[j];
+        // link to their CAO scoring page
         table_string += "<td>" + player_name.link("http://crawl.akrasiac.org/scoring/players/" + player_name.toLowerCase() + ".html") + "</td>";
       }
+      // the idle string is stored as seconds, we want to put it in ##:## format
       else if(j == "Idle")
       {
         table_string += "<td>" + convertIdle(game[j]) + "</td>";
       }
+      // if playing webtiles, we want to add a link to watch the game
       else if(j == "Viewers" && game["Server"] == "CDO-Web")
       {
         table_string += "<td>" + game[j] + " (" + "Watch".link("https://tiles.crawl.develz.org/#watch-" + player_name.toLowerCase()) + ")</td>";
       }
+      // we don't want to see the term column
       else if(!(j == "Term"))
       {
         table_string += "<td>" + game[j] + "</td>";
@@ -215,10 +260,16 @@ function CreateTable()
     table_string += "</tr>";
   }
   
-  table_string += "</table><table><tr onclick='ReloadTable();' onmousedown='return false;' onselectstart='return false;'><td id='players'>" + (game_data.length) + " game" + (game_data.length==1 ? "" : "s") + " in progress</td><td id='timer'></td></tr></table>";
+  // close off the table and add the lower table
+  // clicking this lower table reloads the upper table
+  // two fields: "players" shows the number of games being played,
+  // "timer" shows the countdown timer
+  table_string += "</table><table id='status-table'><tr onclick='ReloadTable();' onmousedown='return false;' onselectstart='return false;'><td id='players'>" + (game_data.length) + " game" + (game_data.length==1 ? "" : "s") + " in progress</td><td id='timer'></td></tr></table>";
   
+  // push the defined table to the page
   document.getElementById('ajax-response').innerHTML = table_string;
   
+  // begin to update the countdown timer
   DrawCountdownTimer();
 }
 
@@ -314,50 +365,6 @@ function DrawCountdownTimer()
     
     timer_string += "</span>";
   }
-  
-  // switch(countdown_timer)
-  // {
-  // case 24:
-    // timer_string = "<span class='blue'>========================</span>";
-    // break;
-  // case 22:
-    // timer_string = "<span class='blue'>======================</span><span class='purple'>--</span>";
-    // break;
-  // case 20:
-    // timer_string = "<span class='blue'>====================</span><span class='purple'>--</span><span class='grey'>--</span>";
-    // break;
-  // case 18:
-    // timer_string = "<span class='blue'>==================</span><span class='purple'>--</span><span class='grey'>----</span>";
-  // case 16:
-    // timer_string = "<span class='blue'>================</span><span class='purple'>--</span><span class='grey'>------</span>";
-    // break;
-  // case 14:
-    // timer_string = "<span class='blue'>==============</span><span class='purple'>--</span><span class='grey'>--------</span>";
-    // break;
-  // case 12:
-    // timer_string = "<span class='blue'>============</span><span class='purple'>--</span><span class='grey'>----------</span>";
-    // break;
-  // case 10:
-    // timer_string = "<span class='blue'>==========</span><span class='purple'>--</span><span class='grey'>------------</span>";
-    // break;
-  // case 8:
-    // timer_string = "<span class='blue'>========</span><span class='purple'>--</span><span class='grey'>--------------</span>";
-    // break;
-  // case 6:
-    // timer_string = "<span class='blue'>======</span><span class='purple'>--</span><span class='grey'>----------------</span>";
-    // break;
-  // case 4:
-    // timer_string = "<span class='blue'>====</span><span class='purple'>--</span><span class='grey'>------------------</span>";
-    // break;
-  // case 2:
-    // timer_string = "<span class='blue'>==</span><span class='purple'>--</span><span class='grey'>--------------------</span>";
-    // break;
-  // case 0:
-    // timer_string = "<span class='purple'>--</span><span class='grey'>----------------------</span>";
-    // break;
-  // default:
-    // break;
-  // }
 
   document.getElementById('timer').innerHTML = timer_string;
 }
