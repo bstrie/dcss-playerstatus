8191b7db7c1888c69e6031cd48d57777b593091d
Ben Striegel <ben.striegel@gmail.com>
2012-08-13 15:38:45 -0400

    Gracefully handle broken status pages and add CSZO
    Also general comment/whitespace cleanup

--- fetch.php
+++ fetch.php
@@ -4,7 +4,7 @@ $tag = $_GET['tag'];  // Server ID
 
 $data = file_get_contents($src);
 
-/* 
+/*
 The dgl-status output that we receive looks like this:
 
 blackcustard#dcss-svn#L12 SpAs, Lair:1#80x24#2230#0#
@@ -15,32 +15,32 @@ ZChris13#spr-svn##80x24#1730#0#
 We want to transform it in the following ways:
 1. Insert the server tag at the end of each record.
 2. Split out the third field into three subfields.
-3. Transform each server's idiosyncratic game/version identifiers (found in 
+3. Transform each server's idiosyncratic game/version identifiers (found in
    field 2) into a more uniform representation.
 4. Split out the game/version identifiers into two fields.
 5. Get rid of the termsize field.
 6. Remove the L from the XL field.
 
-This will yield a string that is still easily parseable. If we're careful, and 
-if we know the characteristics of the dgl-status output, we can do this with 
-string replacement and regexes. 
+This will yield a string that is still easily parseable. If we're careful, and
+if we know the characteristics of the dgl-status output, we can do this with
+string replacement and regexes. This is hilariously fragile! If only the
+servers could respond in JSON, using standardized formats... but such is life.
 */
 
 // We'll use | as our record delimiter as it won't appear in the dgl output.
 $data = str_replace("\n", ($tag . '|'), $data);
 // Field three represents three data points: xl, char, and place.
 $data = str_replace(", ", "#", $data);  // ", " separates char and place
-$data = str_replace("  ", " ", $data);  // If two spaces delimit xl and char 
+$data = str_replace("  ", " ", $data);  // If two spaces delimit xl and char
 $data = str_replace(" ", "#", $data);  // The space between xl and char
-$data = str_replace("##", "####", $data);  // If field three was blank
+$data = str_replace("##", "####", $data);  // If field three was totally blank
 // If only servers could agree on compact, uniform game/version names...
 $data = str_replace("-svn", "-git", $data);
-$data = str_replace("#dcss-", "#dcss-", $data);
 $data = str_replace("#Crawl-", "#dcss-", $data);
-$data = str_replace("#Sprint-", "#spr-", $data); 
+$data = str_replace("#Sprint-", "#spr-", $data);
 $data = str_replace("#ZD-", "#zd-", $data);
 // CDO sends us "spr-0.8" but we want "0.8-spr"
-$pattern = "/#(dcss|spr|zd|tut)-([\d\.a-zA-Z]{3,4})#/"; 
+$pattern = "/#(dcss|spr|zd|tut)-([\d\.a-zA-Z]{3,4})#/";
 $replacement = "#$2#$1#";
 $data = preg_replace($pattern, $replacement, $data);
 // We hate termsize!
@@ -52,20 +52,32 @@ $pattern = "/#L(\d{1,2})#/";
 $replacement = "#$1#";
 $data = preg_replace($pattern, $replacement, $data);
 // Bonus round
-$data = str_replace("#Shoals:", "#Shoal:", $data);  
+$data = str_replace("#Shoals:", "#Shoal:", $data);
 $data = substr($data, 0, strlen($data)-1);  // Chop off the last pipe character
 
 /*
 So now we have something that looks like this:
 
-Chapayev#0.9#dcss#12#NaVM#Lair:6#2#0#CAO/DGL|crate#git#dcss#3#OgBe#D:2#0#0#CAO/DGL|demonblade#git##dcss###1915#0#CAO/DGL
+Chapayev#0.9#dcss#12#NaVM#Lair:6#2#0#CAO/Term|crate#git#dcss#3#OgBe#D:2#0#0#CAO/Term|demonblade#git##dcss###1915#0#CAO/Term
 
 Let's transform it into JSON.
 */
 
 $data = str_replace('#', '","', $data);
 $data = str_replace('|', '"],["', $data);
-$data = '["' . $data . '"]';
+$data = '[["' . $data . '"]]';
+
+/*
+In the event that we can't connect to the remote data source, $data will end up
+as '[[""]]' and PHP may or may not decide to echo an error message (there is no
+way to prevent this behavior, as far as I know, short of reconfiguring PHP). If
+it echoes an error message, the client will catch the error when it attempts to
+parse invalid JSON, so no harm. If it doesn't echo an error message, we'd
+rather return an empty string (and thereby also trigger a failure in the
+parser) rather than return an empty JSON array. We'd love it if PHP could be
+guaranteed to not insert garbage into its responses, but such is life.
+*/
+if ($data == '[[""]]') { $data = ''; }
 
 echo $data;
 ?>
--- playerstatus.js
+++ playerstatus.js
@@ -1,12 +1,16 @@
 playerstatus = (function() {
 
-var URLS = [{'src': 'http://crawl.develz.org/cgi-bin/dgl-status/index.html',
-             'tag': 'CDO/DGL'},
-            {'src': 'http://crawl.develz.org/cgi-bin/web-status/index.html',
-             'tag': 'CDO/Web'},
-            {'src': 'http://crawl.akrasiac.org/cgi-bin/dgl-status/index.html',
-             'tag': 'CAO/DGL'}];
-var PLAYER = 0, 
+var TARGETS = [{'src': 'http://crawl.develz.org/cgi-bin/dgl-status/index.html',
+                'tag': 'CDO/Term'},
+               {'src': 'http://crawl.develz.org/cgi-bin/web-status/index.html',
+                'tag': 'CDO/Web'},
+               {'src': 'http://crawl.akrasiac.org/cgi-bin/dgl-status/index.html',
+                'tag': 'CAO/Term'},
+               {'src': 'http://dobrazupa.org/cgi-bin/dgl-status',
+                'tag': 'CßO/Both'}];
+var WATCH_URLS = {'CDO/Web': 'https://tiles.crawl.develz.org/#watch-',
+                  'CßO/Both': 'http://crawl.s-z.org/#watch-'};
+var PLAYER = 0,
     VER = 1,
     GAME = 2,
     XL = 3,
@@ -14,37 +18,37 @@ var PLAYER = 0,
     PLACE = 5,
     IDLE = 6,
     VIEWERS = 7,
-    SERVER = 8; 
+    SERVER = 8;
 
-function fetchData() {
-    var results = '[';
+function fetchPlayerData() {
+    var results = [];
     var requests = 0;
 
-    $.each(URLS, function(url) {
-        $.get('fetch.php', URLS[url], function(data) {
-            results += data;
-            requests += 1;
-
-            if (requests === URLS.length) {
-                results += ']';
-                formatData(JSON.parse(results));
+    $.each(TARGETS, function(i) {
+        $.get('fetch.php', TARGETS[i], function(data) {
+            if (isJSON(data, TARGETS[i])) {
+                $.merge(results, JSON.parse(data));
             }
-            else {
-                results += ',';
+
+            // These GETs are async. Proceed only once they're all finished
+            requests += 1;
+            if (requests === TARGETS.length) {
+                formatData(results);
             }
         });
     });
 }
 
 function formatData(data) {
+    // Sort by player name
     data.sort(function(a, b) {
         return a[0].toLowerCase() < b[0].toLowerCase() ? -1 : 1;
     });
 
     var fmtdata = $.extend(true, [], data);  // Recursively copy the array
-    // Each array in data looks like this:
+    // Each array in `data` looks like this:
     //  Player      Ver   Game   XL   Char   Place     Idle  Vwr Server
-    // ["DrPraetor","git","dcss","10","OpEE","Volcano","722","0","CAO/DGL"]
+    // ["DrPraetor","git","dcss","10","OpEE","Volcano","722","0","CAO/Term"]
     for (var i1=0; i1<data.length; i1++) {
         fmtdata[i1][PLAYER] = formatPlayer(data[i1]);  // Link to player page
         fmtdata[i1][IDLE] = formatIdle(data[i1]);  // Turn seconds into 00:00
@@ -56,25 +60,30 @@ function formatData(data) {
 
 function drawTable(data) {
     var tablehtml = '<span id="games">' +
-                    data.length + ' games in progress' +
+                      data.length + ' games in progress' +
                     '</span>' +
-                    '<span id="tip">Tip: shift-click on column headings ' +
-                    'to sort by multiple columns at once.</span>' +
-                    '<table id="statustable" class="tablesorter">' +
-                    '<thead><tr>' +
-                    '<th>Player</th>' +
-                    '<th>Ver</th>' +
-                    '<th>Game</th>' +
-                    '<th>XL</th>' +
-                    '<th>Char</th>' +
-                    '<th>Place</th>' +
-                    '<th>Idle</th>' +
-                    '<th>Viewers</th>' +
-                    '<th>Server</th>' +
-                    '</tr></thead><tbody>';
+                    '<span id="tip">' +
+                      'Tip: shift-click on column headings ' +
+                      'to sort by multiple columns at once.' +
+                    '</span>' +
+                      '<table id="statustable" class="tablesorter">' +
+                        '<thead><tr>' +
+                          '<th>Player</th>' +
+                          '<th>Ver</th>' +
+                          '<th>Game</th>' +
+                          '<th>XL</th>' +
+                          '<th>Char</th>' +
+                          '<th>Place</th>' +
+                          '<th>Idle</th>' +
+                          '<th>Viewers</th>' +
+                          '<th>Server</th>' +
+                        '</tr></thead>' +
+                        '<tbody>';
 
+    // Each row of the table body
     for (var i1=0; i1<data.length; i1++) {
         tablehtml += '<tr>';
+        // Each column
         for (var i2=0; i2<data[i1].length; i2++) {
             tablehtml += '<td>' + data[i1][i2] + '</td>';
         }
@@ -83,24 +92,29 @@ function drawTable(data) {
 
     tablehtml += '</tbody></table>';
 
+    // By default, sort on first column (player name)
     var sort = [[0,0]];
+    // Remember our sort order across updates
     if ($('#statustable').length > 0 && $('#statustable')[0].config) {
        sort = $('#statustable')[0].config.sortList;
     }
 
     $('#playerstatus').html(tablehtml);
+
+    // Need to make sure there's room for the sort icons.
     // Kludgy, but the best way to ensure a certain width for each th,
     // since width in ex/em is insufficient and px will vary by font.
-    $('#statustable th').each(function() { 
+    $('#statustable th').each(function() {
         $(this).text($(this).text() + '\u00A0\u00A0')
     });
+
     $('#statustable').tablesorter({
-        // Sort on the first column, ascending
         sortList: sort,
-        headers: {7: {sorter: 'viewers'}}
+        headers: {7: {sorter: 'viewers'}}  // Sort Viewers with custom parser
     });
 
-    setTimeout(fetchData, 30000);
+    // Do it all over again every 30 secs
+    setTimeout(fetchPlayerData, 30000);
 }
 
 function formatPlayer(datum) {
@@ -112,22 +126,25 @@ function formatPlayer(datum) {
            '</a>';
 }
 
+// Idle time is received as elapsed seconds, so we put it in 00:00 format.
+// Sorting assumes no idle time greater than 99:99 (server should d/c by then)
 function formatIdle(datum) {
     var minutes = Math.floor(datum[IDLE] / 60);
     var seconds = datum[IDLE] % 60;
 
     if (minutes < 10) minutes = '0' + minutes;
     if (seconds < 10) seconds = '0' + seconds;
- 
+
     return minutes + ':' + seconds;
 }
 
+// Add an entry to the WATCH_URLS variable to automatically add new Watch links
 function formatViewers(datum) {
-    if (datum[SERVER] === 'CDO/Web') {
+    if (WATCH_URLS[datum[SERVER]] !== undefined) {
         return datum[VIEWERS] +
                ' [' +
                '<a href="' +
-               'https://tiles.crawl.develz.org/#watch-' +
+               WATCH_URLS[datum[SERVER]] +
                datum[PLAYER].toLowerCase() +
                '">' +
                'Watch' +
@@ -139,15 +156,29 @@ function formatViewers(datum) {
     }
 }
 
+// Returns false when a server has no players or when a status page is broken
+function isJSON(str, target) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        console.log(e);
+        console.log('...while parsing data from ' + target['src'] +
+                    ' (' + target['tag'] + ')');
+        return false;
+    }
+    return true;
+}
+
 $(document).ready(function() {
     $('#playerstatus').text('Retrieving data');
+    // A custom parser for sorting the "Viewers" column
     $.tablesorter.addParser({
         id: 'viewers',
         is: function(s) { return false; },
         format: function(s) { return parseInt(s.split(' ')[0]); },
         type: 'numeric'
     });
-    fetchData();
+    fetchPlayerData();  // Initiate the loop
 });
 
 })();
